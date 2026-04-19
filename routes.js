46fd117329ebdb373d6d58ef2e33f89664d7bd2c
Kelwin Esechiel <kelwin_esechiel@hotmail.com>
2026-04-19 17:57:48 -0300

    feat: add pg database

--- routes.js
+++ routes.js
@@ -12,13 +12,13 @@ router.delete("/users/:id", remove)
 
 router.get("/gifts", async (req, res) => {
   try {
-    const result = await pool.query("SELECT * FROM cgifts")
-    res.json(result.rows)
+    const test = await pool.query("SELECT NOW()");
+    res.json(test.rows);
   } catch (error) {
-    console.error("ERRO NO /gifts:", error)
-    res.status(500).json({ error: error.message })
+    console.error("ERRO REAL:", error);
+    res.status(500).json({ error: error.message });
   }
-})
+});
 
 router.post("/gifts/select", async (req, res) => {
   const { id } = req.body
